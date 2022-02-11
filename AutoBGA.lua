b65f4268724b2fba5db5fb3b920b6e03f23586f2
ferib <lopke123@live.nl>
2022-02-11 19:54:01 +0100

    init

---
+++ AutoBGA.lua
@@ -0,0 +1,103 @@
+-- create frame
+local frame = CreateFrame("frame")
+local lastTick = 0
+local lastAutoJoin = 0
+
+local function tick()
+	-- tick on 0.5s interval
+	local tick = GetTime()
+	if lastTick + 0.5 > tick then return end
+	lastTick = tick
+	
+	-- get Queued info
+	for i=1, 10 do
+		local status = GetBattlefieldStatus(i)
+		if status == "none" then
+			-- Do nothing
+		elseif status == "active" then
+			if not CompactRaidFrameManagerDisplayFrameEveryoneIsAssistButton:GetChecked() then 
+				AutoAssistAll()
+				return
+			end
+		elseif status == "queued" then
+			-- wait for pop
+		elseif status == "confirm" then
+			-- enter battleground 
+			if tick > lastAutoJoin + (10*60) then
+				_AcceptBattlefieldPort(i) -- TODO fix
+			end
+			return
+		end
+	end
+end
+
+-- handles AcceptBattlefieldPort for untainted Lua
+local function _AcceptBattlefieldPort(index)
+	_accepted = false
+	local attempts = 0
+	while not _accepted do
+		if attempts > 420 then
+			-- failed to auto-join (tainted)
+			lastAutoJoin = tick -- prevent spam
+			ABAPrint("Battelground ready"); --, please join ASAP to increase the chance to be leader!")
+		end
+
+		attempts = attempts + 1
+		print("test: " .. attempts)
+		RunScript([[
+		if not issecure() then return end
+		--JumpOrAscendStart()
+		AcceptBattlefieldPort(]] .. index .. [[, true)
+		_accepted = true;]])
+		
+		-- done
+		if _accepted then
+			-- the faster we join, the more chance we be lead!
+			lastAutoJoin = tick -- prevent spam
+			ABAPrint("Auto joined battleground!")
+			return
+		end
+	end
+end
+
+-- check for assist all if not already all assist
+local function AutoAssistAll()
+	local playername = UnitName("player")
+	for i=1, 40 do
+		local name, rank, subgroup, level, class, fileName, zone, online, isDead, role, isML = GetRaidRosterInfo(i);
+		if name == playername and rank == 2 then
+			CompactRaidFrameManagerDisplayFrameEveryoneIsAssistButton:Click()
+			ABAPrint("Auto Assisted Everyone!", true)
+		end
+	end
+end
+
+-- fancy print
+local function ABAPrint(str, p)
+	local public = p or false
+	print("|cfff5deb3[AutoBGA]|r: " .. str)	
+	if p then
+		SendChatMessage("[AutoBGA]: " .. str, "SAY")
+	end
+end
+
+-- register to frame
+local function init()
+	-- register tick
+	frame:SetScript("OnUpdate", tick)
+	
+	-- register events
+	frame:SetScript("OnEvent", function(self, event, ...)
+		ABAPrint(event)
+		StaticPopup1:Hide() -- No need to tell me!
+	end);		
+	frame:RegisterEvent("MACRO_ACTION_BLOCKED");
+	frame:RegisterEvent("ADDON_ACTION_FORBIDDEN");
+	frame:RegisterEvent("MACRO_ACTION_BLOCKED");
+	frame:RegisterEvent("ADDON_ACTION_FORBIDDEN");
+	
+	-- say hello!
+	ABAPrint("Auto Battleground Assist is Ready!")
+end
+
+C_Timer.After(2.25, init)
---
+++ readme.md
@@ -0,0 +1,24 @@
+# Auto BG Assist
+
+Basic AddOn for World of Warcraft *(TBCC)* to automatically enable 'Assist Everyone' in Battelground.
+
+Features:
+- [x] Auto Join Battelgrounds*
+- [x] Auto Assist Everyone *(if leader)*
+
+The idea is to make everyone automatically assisant in the BG group as this will improve the quality of the BG.
+
+To be able to assist everyone, you need to be group leader.
+
+#### How to become group leader?
+
+From my experience the first players that joins the BG instance will become leader, however when a new player joins you might lose your leadeship *(this seem random?)*
+
+This AddOn allows you to nearly instantly join a BG instance, increasing the change of being the leader.
+Additionally it will automatically set the 'Assist Everyone' nearly instantly so that you dont have to worry about losing your leadeship once another player joins.
+
+**Please spread the word,** as I am tired of `/w` people asking for assist!
+
+## How To Use
+
+Create a new folder named `AutoBG` at `World of Warcraft\_classic_\Interface\AddOns\` and move the files into there.
